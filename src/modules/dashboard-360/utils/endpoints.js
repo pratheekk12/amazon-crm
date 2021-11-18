5a18fe44355459082d95936bbf9a2fea1af91ea2
Pratheekk12 <pratheek@grassrootsbpo.in>
2021-11-18 10:25:38 +0530

    proxy settings

--- src/modules/dashboard-360/utils/endpoints.js
+++ src/modules/dashboard-360/utils/endpoints.js
@@ -1,13 +1,18 @@
-export const AUTH = 'http://192.168.3.37:63003/api'
-export const AGENT_SERVICE = 'http://192.168.3.37:63000/api'
-export const AMI = 'http://192.168.3.37:63001/ami'
-export const SAVE_DISPOSITION = '/'
+// export const AUTH = 'http://192.168.3.37:63003/api'
+// export const AGENT_SERVICE = 'http://192.168.3.37:63000/api'
+// export const AMI = 'http://192.168.3.37:63001/ami'
 
-export const CAMPAIGN = '/fileupload'
-export const CAMPAIGN_REPORT = '/fileupload'
-export const UPLOAD_FILE = '/fileupload'
+export const AUTH = '/otp_auth/api'
+export const AGENT_SERVICE = 'otp_agentservice/api'
+export const AMI = '/otp_ami/ami'
 
-export const AGENT_PERFORMANCE = '/agentservice'
+// export const SAVE_DISPOSITION = '/'
+
+// export const CAMPAIGN = '/fileupload'
+// export const CAMPAIGN_REPORT = '/fileupload'
+// export const UPLOAD_FILE = '/fileupload'
+
+// export const AGENT_PERFORMANCE = '/agentservice'
 
 
 
